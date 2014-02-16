f39261eb88a59deda5f185d8357faeb1c0a7b913
Wenbin Dai <dwb1983@gmail.com>
2014-02-15 18:32:50 -0800

    srm 609

---
+++ TopCoder/SRM/609/PackingBallsDiv1.cpp
@@ -0,0 +1,180 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+// BEGIN CUT HERE
+#define ARRSIZE(x) (sizeof(x)/sizeof(x[0]))
+
+template<typename T> void print( T a ) {
+    cerr << a;
+}
+static void print( long long a ) {
+    cerr << a << "L";
+}
+static void print( string a ) {
+    cerr << '"' << a << '"';
+}
+template<typename T> void print( vector<T> a ) {
+    cerr << "{";
+    for ( int i = 0 ; i != a.size() ; i++ ) {
+        if ( i != 0 ) cerr << ", ";
+        print( a[i] );
+    }
+    cerr << "}" << endl;
+}
+template<typename T> void eq( int n, T have, T need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+template<typename T> void eq( int n, vector<T> have, vector<T> need ) {
+    if( have.size() != need.size() ) {
+        cerr << "Case " << n << " failed: returned " << have.size() << " elements; expected " << need.size() << " elements.";
+        print( have );
+        print( need );
+        return;
+    }
+    for( int i= 0; i < have.size(); i++ ) {
+        if( have[i] != need[i] ) {
+            cerr << "Case " << n << " failed. Expected and returned array differ in position " << i << ".";
+            print( have );
+            print( need );
+            return;
+        }
+    }
+    cerr << "Case " << n << " passed." << endl;
+}
+static void eq( int n, string have, string need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+// END CUT HERE
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+#define FOREACH(it,c) for(typeof((c).begin())it=(c).begin();it!=(c).end();++it)
+#define CLR(x) memset((x),0,sizeof((x)))
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+// BEGIN CUT HERE
+vector<string> split( const string& s, const string& delim =" " ) {
+    vector<string> res;
+    string t;
+    for ( int i = 0 ; i != s.size() ; i++ ) {
+        if ( delim.find( s[i] ) != string::npos ) {
+            if ( !t.empty() ) {
+                res.push_back( t );
+                t = "";
+            }
+        } else {
+            t += s[i];
+        }
+    }
+    if ( !t.empty() ) {
+        res.push_back(t);
+    }
+    return res;
+}
+
+vector<int> splitInt( const string& s, const string& delim =" " ) {
+    vector<string> tok = split( s, delim );
+    vector<int> res;
+    for ( int i = 0 ; i != tok.size(); i++ )
+        res.push_back( atoi( tok[i].c_str() ) );
+    return res;
+}
+// END CUT HERE
+
+// BEGIN CUT HERE
+int s2i(string s) {
+    stringstream ss;
+    ss << s;
+    int res;
+    ss >> res;
+    return res;
+}
+
+string i2s(int n) {
+    stringstream ss;
+    ss << n;
+    string res;
+    ss >> res;
+    return res;
+}
+// END CUT HERE
+
+class PackingBallsDiv1 {
+public:
+    int minPacks(int K, int A, int B, int C, int D) {
+        vector<LL> mm;
+        mm.push_back(A);
+        FOR(i,1,K-1) {
+            mm.push_back(mm[i - 1]);
+            mm[i] = (mm[i] * B + C) % D + 1;
+        }
+        int res = 0;
+        REP(i,K) {
+            res += mm[i] / K;
+            mm[i] %= K;
+        }
+        sort(mm.begin(), mm.end());
+        LL ans = res + K;
+        REP(i,mm.size()) {
+            ans = min(ans, res + K - 1 - i + mm[i]);
+        }
+        return ans;
+    }
+};
+// BEGIN CUT HERE
+int main() {
+    {
+        PackingBallsDiv1 theObject;
+        eq(0, theObject.minPacks(3, 4, 2, 5, 6),4);
+    }
+    {
+        PackingBallsDiv1 theObject;
+        eq(1, theObject.minPacks(1, 58, 23, 39, 93),58);
+    }
+    {
+        PackingBallsDiv1 theObject;
+        eq(2, theObject.minPacks(23, 10988, 5573, 4384, 100007),47743);
+    }
+    {
+        PackingBallsDiv1 theObject;
+        eq(3, theObject.minPacks(100000, 123456789, 234567890, 345678901, 1000000000),331988732);
+    }
+    
+    return 0;
+}
+// END CUT HERE
