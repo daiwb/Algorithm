3c870b352b0fbd414cfba4e7a8624615c44f578d
Wenbin Dai <dwb1983@gmail.com>
2014-03-06 19:13:58 -0800

    leetcode 3/6/2014

---
+++ LeetCode/reverseWords.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+typedef long long LL;
+
+vector<string> split( const string& s, const string& delim =" " ) {
+    vector<string> res;
+    string t;
+    for ( int i = 0 ; i != s.size() ; i++ ) {
+        if ( delim.find( s[i] ) != string::npos ) {
+            if ( !t.empty() ) {
+                res.push_back( t );
+                t = "";
+            }
+        } else {
+            t += s[i];
+        }
+    }
+    if ( !t.empty() ) {
+        res.push_back(t);
+    }
+    return res;
+}
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        vector<string> mm = split(s);
+        s = "";
+        RFOR(i,mm.size()-1,0) {
+            if (i != mm.size() - 1) s += " ";
+            s += mm[i];
+        }
+    }
+};
+
+int main() {
+    return 0;
+}
