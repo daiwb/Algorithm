cb77f10c5ba8be2cd20d03556d711736c16bc893
Wenbin Dai <dwb1983@gmail.com>
2014-05-17 20:10:25 -0700

    TCO14 R2A

---
+++ TopCoder/Tournaments/TCO14/R2A/SixteenBricks.cpp
@@ -0,0 +1,167 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+// BEGIN CUT HERE
+#define ARRSIZE(x) (sizeof(x)/sizeof(x[0]))
+
+template<typename T> void print( T a ) {
+    cerr << a;
+}
+static void print( long long a ) {
+    cerr << a << "L";
+}
+static void print( string a ) {
+    cerr << '"' << a << '"';
+}
+template<typename T> void print( vector<T> a ) {
+    cerr << "{";
+    for ( int i = 0 ; i != a.size() ; i++ ) {
+        if ( i != 0 ) cerr << ", ";
+        print( a[i] );
+    }
+    cerr << "}" << endl;
+}
+template<typename T> void eq( int n, T have, T need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+template<typename T> void eq( int n, vector<T> have, vector<T> need ) {
+    if( have.size() != need.size() ) {
+        cerr << "Case " << n << " failed: returned " << have.size() << " elements; expected " << need.size() << " elements.";
+        print( have );
+        print( need );
+        return;
+    }
+    for( int i= 0; i < have.size(); i++ ) {
+        if( have[i] != need[i] ) {
+            cerr << "Case " << n << " failed. Expected and returned array differ in position " << i << ".";
+            print( have );
+            print( need );
+            return;
+        }
+    }
+    cerr << "Case " << n << " passed." << endl;
+}
+static void eq( int n, string have, string need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+// END CUT HERE
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+
+// BEGIN CUT HERE
+vector<string> split( const string& s, const string& delim =" " ) {
+    vector<string> res;
+    string t;
+    for ( int i = 0 ; i != s.size() ; i++ ) {
+        if ( delim.find( s[i] ) != string::npos ) {
+            if ( !t.empty() ) {
+                res.push_back( t );
+                t = "";
+            }
+        } else {
+            t += s[i];
+        }
+    }
+    if ( !t.empty() ) {
+        res.push_back(t);
+    }
+    return res;
+}
+
+vector<int> splitInt( const string& s, const string& delim =" " ) {
+    vector<string> tok = split( s, delim );
+    vector<int> res;
+    for ( int i = 0 ; i != tok.size(); i++ )
+        res.push_back( atoi( tok[i].c_str() ) );
+    return res;
+}
+// END CUT HERE
+
+// BEGIN CUT HERE
+int s2i(string s) {
+    stringstream ss;
+    ss << s;
+    int res;
+    ss >> res;
+    return res;
+}
+
+string i2s(int n) {
+    stringstream ss;
+    ss << n;
+    string res;
+    ss >> res;
+    return res;
+}
+// END CUT HERE
+
+class SixteenBricks {
+public:
+    int maximumSurface(vector <int> height) {
+        sort(height.begin(), height.end());
+        int res = 16;
+        REP(i,16) res += height[i] * 4;
+        FOR(i,0,1) res -= height[i] * 4 * 2;
+        FOR(i,2,5) res -= height[i] * 3 * 2;
+        FOR(i,6,7) res -= height[i] * 2 * 2;
+        return res;
+    }
+};
+// BEGIN CUT HERE
+int main() {
+    {
+        int heightARRAY[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        vector <int> height( heightARRAY, heightARRAY+ARRSIZE(heightARRAY) );
+        SixteenBricks theObject;
+        eq(0, theObject.maximumSurface(height),32);
+    }
+    {
+        int heightARRAY[] = {1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2};
+        vector <int> height( heightARRAY, heightARRAY+ARRSIZE(heightARRAY) );
+        SixteenBricks theObject;
+        eq(1, theObject.maximumSurface(height),64);
+    }
+    {
+        int heightARRAY[] = {77, 78, 58, 34, 30, 20, 8, 71, 37, 74, 21, 45, 39, 16, 4, 59}
+           ;
+        vector <int> height( heightARRAY, heightARRAY+ARRSIZE(heightARRAY) );
+        SixteenBricks theObject;
+        eq(2, theObject.maximumSurface(height),1798);
+    }
+    return 0;
+}
+// END CUT HERE
