600309c462382e2fd3b3bce346eaa1fcc9cf6b0b
Wenbin Dai <dwb1983@gmail.com>
2014-07-10 11:15:14 -0700

    SRM 627

---
+++ TopCoder/SRM/627/GraphInversions.cpp
@@ -0,0 +1,242 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+// BEGIN CUT HERE
+#define ARRSIZE(x) (sizeof(x)/sizeof(x[0]))
+
+template<typename T> void print( T a ) {
+    cerr << a;
+}
+static void print( long long a ) {
+    cerr << a << "L";
+}
+static void print( string a ) {
+    cerr << '"' << a << '"';
+}
+template<typename T> void print( vector<T> a ) {
+    cerr << "{";
+    for ( int i = 0 ; i != a.size() ; i++ ) {
+        if ( i != 0 ) cerr << ", ";
+        print( a[i] );
+    }
+    cerr << "}" << endl;
+}
+template<typename T> void eq( int n, T have, T need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+template<typename T> void eq( int n, vector<T> have, vector<T> need ) {
+    if( have.size() != need.size() ) {
+        cerr << "Case " << n << " failed: returned " << have.size() << " elements; expected " << need.size() << " elements.";
+        print( have );
+        print( need );
+        return;
+    }
+    for( int i= 0; i < have.size(); i++ ) {
+        if( have[i] != need[i] ) {
+            cerr << "Case " << n << " failed. Expected and returned array differ in position " << i << ".";
+            print( have );
+            print( need );
+            return;
+        }
+    }
+    cerr << "Case " << n << " passed." << endl;
+}
+static void eq( int n, string have, string need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+// END CUT HERE
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+#define CLR(x) memset((x),0,sizeof((x)))
+#define SORT(x) sort(x.begin(), x.end())
+#define SZ(x) x.size()
+#define PB push_back
+typedef long long LL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+
+// BEGIN CUT HERE
+vector<string> split( const string& s, const string& delim =" " ) {
+    vector<string> res;
+    string t;
+    for ( int i = 0 ; i != s.size() ; i++ ) {
+        if ( delim.find( s[i] ) != string::npos ) {
+            if ( !t.empty() ) {
+                res.push_back( t );
+                t = "";
+            }
+        } else {
+            t += s[i];
+        }
+    }
+    if ( !t.empty() ) {
+        res.push_back(t);
+    }
+    return res;
+}
+
+vector<int> splitInt( const string& s, const string& delim =" " ) {
+    vector<string> tok = split( s, delim );
+    vector<int> res;
+    for ( int i = 0 ; i != tok.size(); i++ )
+        res.push_back( atoi( tok[i].c_str() ) );
+    return res;
+}
+// END CUT HERE
+
+// BEGIN CUT HERE
+int s2i(string s) {
+    stringstream ss;
+    ss << s;
+    int res;
+    ss >> res;
+    return res;
+}
+
+string i2s(int n) {
+    stringstream ss;
+    ss << n;
+    string res;
+    ss >> res;
+    return res;
+}
+// END CUT HERE
+
+#define INF 1000000000
+
+int n, k, res;
+VVI mm;
+VI val;
+int used[1005], que[1005];
+
+class GraphInversions {
+public:
+    void dfs(int cur, int num, int sum) {
+        if (sum >= res) return;
+        if (num >= k) {
+            res = min(res, sum);
+            return;
+        }
+        REP(i,SZ(mm[cur])) {
+            int next = mm[cur][i];
+            if (used[next] == 1) continue;
+            used[next] = 1;
+            que[num] = next;
+            int add = 0;
+            REP(j,num) {
+                if (val[next] < val[que[j]]) ++add;
+            }
+            dfs(next, num + 1, sum + add);
+            used[next] = 0;
+        }
+    }
+    int getMinimumInversions(vector <int> A, vector <int> B, vector <int> V, int K) {
+        val = V;
+        k = K;
+        n = SZ(A);
+        mm.assign(n, VI());
+        REP(i,n) {
+            int a = A[i], b = B[i];
+            mm[a].PB(b);
+            mm[b].PB(a);
+        }
+        res = INF;
+        REP(i,n) {
+            CLR(used);
+            que[0] = i;
+            used[i] = 1;
+            dfs(i, 1, 0);
+        }
+        if (res == INF) return -1;
+        return res;
+    }
+};
+// BEGIN CUT HERE
+int main() {
+    {
+        int AARRAY[] = {0,1,2};
+        vector <int> A( AARRAY, AARRAY+ARRSIZE(AARRAY) );
+        int BARRAY[] = {1,2,0};
+        vector <int> B( BARRAY, BARRAY+ARRSIZE(BARRAY) );
+        int VARRAY[] = {40,50,60};
+        vector <int> V( VARRAY, VARRAY+ARRSIZE(VARRAY) );
+        GraphInversions theObject;
+        eq(0, theObject.getMinimumInversions(A, B, V, 3),0);
+    }
+    {
+        int AARRAY[] = {0,1,2,3};
+        vector <int> A( AARRAY, AARRAY+ARRSIZE(AARRAY) );
+        int BARRAY[] = {1,2,3,0};
+        vector <int> B( BARRAY, BARRAY+ARRSIZE(BARRAY) );
+        int VARRAY[] = {60,40,50,30};
+        vector <int> V( VARRAY, VARRAY+ARRSIZE(VARRAY) );
+        GraphInversions theObject;
+        eq(1, theObject.getMinimumInversions(A, B, V, 3),1);
+    }
+    {
+        int AARRAY[] = {0,1,2,3,0};
+        vector <int> A( AARRAY, AARRAY+ARRSIZE(AARRAY) );
+        int BARRAY[] = {1,2,3,0,4};
+        vector <int> B( BARRAY, BARRAY+ARRSIZE(BARRAY) );
+        int VARRAY[] = {10,10,10,5,5};
+        vector <int> V( VARRAY, VARRAY+ARRSIZE(VARRAY) );
+        GraphInversions theObject;
+        eq(2, theObject.getMinimumInversions(A, B, V, 5),1);
+    }
+    {
+        int AARRAY[] = {0,1,2,3,0,2};
+        vector <int> A( AARRAY, AARRAY+ARRSIZE(AARRAY) );
+        int BARRAY[] = {1,2,3,0,4,5};
+        vector <int> B( BARRAY, BARRAY+ARRSIZE(BARRAY) );
+        int VARRAY[] = {10,2,5,3,7,1};
+        vector <int> V( VARRAY, VARRAY+ARRSIZE(VARRAY) );
+        GraphInversions theObject;
+        eq(3, theObject.getMinimumInversions(A, B, V, 6),-1);
+    }
+    {
+        int AARRAY[] = {5,7,7,5,5,7,6,4};
+        vector <int> A( AARRAY, AARRAY+ARRSIZE(AARRAY) );
+        int BARRAY[] = {2,0,2,0,1,4,7,3};
+        vector <int> B( BARRAY, BARRAY+ARRSIZE(BARRAY) );
+        int VARRAY[] = {15,10,5,30,22,10,5,2};
+        vector <int> V( VARRAY, VARRAY+ARRSIZE(VARRAY) );
+        GraphInversions theObject;
+        eq(4, theObject.getMinimumInversions(A, B, V, 6),3);
+    }
+    return 0;
+}
+// END CUT HERE
---
+++ TopCoder/SRM/627/HappyLetterDiv1.cpp
@@ -0,0 +1,203 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+// BEGIN CUT HERE
+#define ARRSIZE(x) (sizeof(x)/sizeof(x[0]))
+
+template<typename T> void print( T a ) {
+    cerr << a;
+}
+static void print( long long a ) {
+    cerr << a << "L";
+}
+static void print( string a ) {
+    cerr << '"' << a << '"';
+}
+template<typename T> void print( vector<T> a ) {
+    cerr << "{";
+    for ( int i = 0 ; i != a.size() ; i++ ) {
+        if ( i != 0 ) cerr << ", ";
+        print( a[i] );
+    }
+    cerr << "}" << endl;
+}
+template<typename T> void eq( int n, T have, T need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+template<typename T> void eq( int n, vector<T> have, vector<T> need ) {
+    if( have.size() != need.size() ) {
+        cerr << "Case " << n << " failed: returned " << have.size() << " elements; expected " << need.size() << " elements.";
+        print( have );
+        print( need );
+        return;
+    }
+    for( int i= 0; i < have.size(); i++ ) {
+        if( have[i] != need[i] ) {
+            cerr << "Case " << n << " failed. Expected and returned array differ in position " << i << ".";
+            print( have );
+            print( need );
+            return;
+        }
+    }
+    cerr << "Case " << n << " passed." << endl;
+}
+static void eq( int n, string have, string need ) {
+    if ( have == need ) {
+        cerr << "Case " << n << " passed." << endl;
+    } else {
+        cerr << "Case " << n << " failed: expected ";
+        print( need );
+        cerr << " received ";
+        print( have );
+        cerr << "." << endl;
+    }
+}
+// END CUT HERE
+
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for(int i=(a);i>=(b);--i)
+#define SORT(x) sort(x.begin(), x.end())
+#define SZ(x) x.size()
+#define MP make_pair
+#define MPI make_pair<int, int>
+#define PB push_back
+typedef long long LL;
+typedef vector<int> VI;
+
+// BEGIN CUT HERE
+vector<string> split( const string& s, const string& delim =" " ) {
+    vector<string> res;
+    string t;
+    for ( int i = 0 ; i != s.size() ; i++ ) {
+        if ( delim.find( s[i] ) != string::npos ) {
+            if ( !t.empty() ) {
+                res.push_back( t );
+                t = "";
+            }
+        } else {
+            t += s[i];
+        }
+    }
+    if ( !t.empty() ) {
+        res.push_back(t);
+    }
+    return res;
+}
+
+vector<int> splitInt( const string& s, const string& delim =" " ) {
+    vector<string> tok = split( s, delim );
+    vector<int> res;
+    for ( int i = 0 ; i != tok.size(); i++ )
+        res.push_back( atoi( tok[i].c_str() ) );
+    return res;
+}
+// END CUT HERE
+
+// BEGIN CUT HERE
+int s2i(string s) {
+    stringstream ss;
+    ss << s;
+    int res;
+    ss >> res;
+    return res;
+}
+
+string i2s(int n) {
+    stringstream ss;
+    ss << n;
+    string res;
+    ss >> res;
+    return res;
+}
+// END CUT HERE
+
+class HappyLetterDiv1 {
+public:
+    string getHappyLetters(string letters) {
+        VI mm(26, 0);
+        int len = letters.size();
+        REP(i,len) {
+            mm[letters[i] - 'a']++;
+        }
+        string res = "";
+
+        REP(ch,26) {
+            if (mm[ch] == 0) continue;
+            bool isok = false;
+            FOR(m,1,mm[ch]) {
+                int cnt = len - m;
+                if (cnt & 1) continue;
+                VI tmp = mm;
+                tmp[ch] = mm[ch] - m;
+                bool flag = true;
+                REP(i,26) {
+                    if (tmp[i] > (cnt / 2)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    isok = true;
+                    break;
+                }
+            }
+            if (isok) {
+                res += char(ch + 'a');
+            }
+        }
+        
+        SORT(res);
+        return res;
+    }
+
+};
+// BEGIN CUT HERE
+int main() {
+    {
+        HappyLetterDiv1 theObject;
+        eq(0, theObject.getHappyLetters("aabbacccc"),"abc");
+    }
+    {
+        HappyLetterDiv1 theObject;
+        eq(1, theObject.getHappyLetters("aaaaaaaccdd"),"a");
+    }
+    {
+        HappyLetterDiv1 theObject;
+        eq(2, theObject.getHappyLetters("ddabccadb"),"abcd");
+    }
+    {
+        HappyLetterDiv1 theObject;
+        eq(3, theObject.getHappyLetters("aaabbb"),"");
+    }
+    {
+        HappyLetterDiv1 theObject;
+        eq(4, theObject.getHappyLetters("rdokcogscosn"),"cos");
+    }
+    return 0;
+}
+// END CUT HERE
